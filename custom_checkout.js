237c33d2802d7683f3d07044a2f7382d2bcb9ec4
Luciano Mallmann <35118768+lucianomallmann@users.noreply.github.com>
2019-02-03 13:38:16 -0200

    Update custom_checkout.js

--- custom_checkout.js
+++ custom_checkout.js
@@ -7,3 +7,6 @@ try{
 document.getElementById('checkout_payment_gateway_28653420644').click();
 document.getElementById("payment-gateway-subfields-17179803748").insertAdjacentHTML("afterbegin","<div class='blank-state'><center><h1>ATENÇÃO!<br><br> Não parcelamos compras com boleto bancário.<br><br> Boleto com prazo de vencimento em 1 dia, só clique em comprar se realmente tiver interesse. <br><br>Este método de pagamento tem uma taxa extra de 5% sobre o valor total da compra.</h1></center></div>");
 }catch(error){};
+
+
+//////////////////////////
